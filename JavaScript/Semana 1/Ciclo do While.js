1906a8d8c723b2629cf1b3b4b8975e5fd402fe74
AgusAdTello <112036990+AgusAdTello@users.noreply.github.com>
2023-05-01 16:21:46 -0300

    Ejercicios
    Modificacion de los ejercicios

---
+++ JavaScript/Semana 1/Ciclo do While.js
@@ -0,0 +1,7 @@
+//do while 
+let conteo = 0;
+do{
+    console.log(conteo);
+    conteo++;
+}while(conteo<3);
+console.log("Fin del ciclo do while");
---
+++ JavaScript/Semana 1/Palabra reservada Break.js
@@ -0,0 +1,8 @@
+//Palabra reservada break
+for(let contando = 0;contando<=10;contando++){
+    if(contando%2==0){
+        console.log(contando);
+        break;
+    }
+}
+console.log("Termina el ciclo al encontrar el primer numero par");

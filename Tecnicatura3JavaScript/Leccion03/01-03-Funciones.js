fcea113e9083aeca204f988242356df98e4ce2c5
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-05-10 14:18:57 -0300

    issue #25 Paso por valor en Js

--- Tecnicatura3JavaScript/Leccion03/01-03-Funciones.js
+++ Tecnicatura3JavaScript/Leccion03/01-03-Funciones.js
@@ -6,3 +6,12 @@ function miFuncion(a, b){
 
 //Llamando la Funcion
 miFuncion(5,4);
+
+// Paso por valor
+// Tipos primitivos
+let k = 10;
+function cambiarValor(a) {
+    a = 20; // variable de uso dentro de la función
+}
+
+cambiarValor(k); // le pasamos solo el valor de k, no la referencia a k, no se va a modificar el valor de k

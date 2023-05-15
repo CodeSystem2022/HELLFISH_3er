3bd098dd0d8e25937ee0688fcca0fe591760199b
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-05-15 11:22:51 -0300

    Conversion de Objetos (Casting) video 1

---
+++ Tecnicatura3Java/Lección05/ConversionObjetos/domain/Empleado.java
@@ -0,0 +1,32 @@
+package domain;
+
+public class Empleado {
+    protected String nombre;
+    protected double sueldo;
+
+    public Empleado(String nombre, double sueldo) {
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+
+    // Método para la sobreescritura
+    public String obtenerDetalles() {
+        return "Nombre: " + this.nombre + ", Sueldo: " + this.sueldo;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public double getSueldo() {
+        return this.sueldo;
+    }
+
+    public void setNombre( String nombre ) {
+        this.nombre = nombre;
+    }
+
+    public void setSueldo(double sueldo){
+        this.sueldo = sueldo;
+    }
+}

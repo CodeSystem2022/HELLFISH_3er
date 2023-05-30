4e9742e9162a4919385cf32d246aa2bfb6637c67
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-05-30 19:47:57 -0300

    video 4 - Método Set

--- Tecnicatura3JavaScript/Leccion05/05-03-classes.js
+++ Tecnicatura3JavaScript/Leccion05/05-03-classes.js
@@ -8,12 +8,32 @@ class Persona {
     get nombre() {
         return this._nombre;
     }
+
+    get apellido() {
+        return this._apellido;
+    }
+
+    set nombre(s) {
+        this._nombre = s;
+    }
+
+    set apellido(s) {
+        this._apellido = s;
+    }
 }
 
 let persona1 = new Persona("Martina", "P");
 // console.log(persona1);
 console.log(persona1.nombre);
+persona1.nombre = "Juana Clara";
+console.log(persona1.nombre);
+persona1.apellido = "M"
+console.log(persona1.nombre + persona1.apellido);
 
 let persona2 = new Persona("Carla", "L");
 // console.log(persona2);
 console.log(persona2.nombre);
+persona2.nombre = "Romina";
+console.log(persona2.nombre);
+persona2.apellido = "S"
+console.log(persona2.nombre + persona2.apellido);

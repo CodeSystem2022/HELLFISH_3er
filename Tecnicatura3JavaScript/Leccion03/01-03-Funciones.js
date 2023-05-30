1cddf5bfb154063d4f4cd43fa6c0f5f0d7c1f371
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-05-30 14:44:24 -0300

    video 2 - Palabra Return

--- Tecnicatura3JavaScript/Leccion03/01-03-Funciones.js
+++ Tecnicatura3JavaScript/Leccion03/01-03-Funciones.js
@@ -1,11 +1,15 @@
 miFuncion(8,2); //Esto se le conoce como hosting
 
 function miFuncion(a, b){
-    console.log("Sumamos: "+ (a + b));
+    // console.log("Sumamos: "+ (a + b));
+    return a + b;
 }
 
 //Llamando la Funcion
 miFuncion(5,4);
+
+let resultado = miFuncion(6, 7);
+console.log(resultado);
 
 // Paso por valor
 // Tipos primitivos

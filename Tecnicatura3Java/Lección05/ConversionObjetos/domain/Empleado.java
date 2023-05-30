1bf437e0c6adabc5a6f95d031e30ebc3c5ae8ff9
SofiaaSottile18 <112595421+SofiaaSottile18@users.noreply.github.com>
2023-05-30 01:02:10 -0300

    Update Empleado.java
    #79 Hashcode y equals

--- Tecnicatura3Java/Lección05/ConversionObjetos/domain/Empleado.java
+++ Tecnicatura3Java/Lección05/ConversionObjetos/domain/Empleado.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class Empleado {
     protected String nombre;
     protected double sueldo;
@@ -29,4 +31,37 @@ public class Empleado {
     public void setSueldo(double sueldo){
         this.sueldo = sueldo;
     }
-}
+ @Override
+    public String toString(){
+        return "Empleado { nombre: " + nombre + ", sueldo: " + sueldo + " }";
+    }
+
+    @Override 
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.nombre);
+        hash = 53 * hash + (int) (Double.doubleToLongBits(this.sueldo) ^ (Double.doubleToLongBits(this.sueldo) >>> 32));
+        return hash; 
+    }
+
+    @Override 
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Empleado other = (Empleado) obj;
+        if (Double.doubleToLongBits(this.sueldo) != Double.doubleToLongBits(other.sueldo)) {
+            return false;
+        }
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        return true; 
+    }
+

32ecb56cc1cf875ae21accece3f041e2d2f6349a
SofiaaSottile18 <112595421+SofiaaSottile18@users.noreply.github.com>
2023-05-30 13:59:35 -0300

    Update Empleado.java

--- Tecnicatura3Java/Lección05/ConversionObjetos/domain/Empleado.java
+++ Tecnicatura3Java/Lección05/ConversionObjetos/domain/Empleado.java
@@ -31,37 +31,3 @@ public class Empleado {
     public void setSueldo(double sueldo){
         this.sueldo = sueldo;
     }
- @Override
-    public String toString(){
-        return "Empleado { nombre: " + nombre + ", sueldo: " + sueldo + " }";
-    }
-
-    @Override 
-    public int hashCode() {
-        int hash = 7;
-        hash = 53 * hash + Objects.hashCode(this.nombre);
-        hash = 53 * hash + (int) (Double.doubleToLongBits(this.sueldo) ^ (Double.doubleToLongBits(this.sueldo) >>> 32));
-        return hash; 
-    }
-
-    @Override 
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Empleado other = (Empleado) obj;
-        if (Double.doubleToLongBits(this.sueldo) != Double.doubleToLongBits(other.sueldo)) {
-            return false;
-        }
-        if (!Objects.equals(this.nombre, other.nombre)) {
-            return false;
-        }
-        return true; 
-    }
-

65ad86992b0715f02f7bf942379d77de0b678bb5
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-05-30 19:43:16 -0300

    Video 2 - Sintaxis de Clases en Js parte 1 y 2

---
+++ Tecnicatura3JavaScript/Leccion05/05-03-classes.js
@@ -0,0 +1,13 @@
+// Clase 06 - 15-05
+class Persona {
+    constructor(nombre, apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+let persona1 = new Persona("Martina", "P");
+console.log(persona1);
+
+let persona2 = new Persona("Carla", "L");
+console.log(persona2);

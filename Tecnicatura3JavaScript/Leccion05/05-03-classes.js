1bcb28955d2d1aea0c7acc8d37cf8c18e1bb6a91
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-05-30 19:44:57 -0300

    video 3 - Método Get

--- Tecnicatura3JavaScript/Leccion05/05-03-classes.js
+++ Tecnicatura3JavaScript/Leccion05/05-03-classes.js
@@ -1,13 +1,19 @@
 // Clase 06 - 15-05
 class Persona {
     constructor(nombre, apellido) {
-        this.nombre = nombre;
-        this.apellido = apellido;
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+
+    get nombre() {
+        return this._nombre;
     }
 }
 
 let persona1 = new Persona("Martina", "P");
-console.log(persona1);
+// console.log(persona1);
+console.log(persona1.nombre);
 
 let persona2 = new Persona("Carla", "L");
-console.log(persona2);
+// console.log(persona2);
+console.log(persona2.nombre);

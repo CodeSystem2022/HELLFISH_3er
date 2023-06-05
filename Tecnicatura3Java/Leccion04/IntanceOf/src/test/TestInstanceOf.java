37d6fa6ea7affd882637912f920816a009178ac9
ludmilailen <aylen-992011@hotmail.com>
2023-06-05 20:13:54 -0300

    Update TestInstanceOf.java

--- Tecnicatura3Java/Leccion04/IntanceOf/src/test/TestInstanceOf.java
+++ Tecnicatura3Java/Leccion04/IntanceOf/src/test/TestInstanceOf.java
@@ -6,15 +6,25 @@ import domain.*;
 public class TestInstanceOf {
     public static void main(String[] args) {
         Empleado empleado1 = new Empleado("Ludmila", 10000);
-        imprimir(empleado1);
-        //System.out.println("empleado1 = " + empleado1.obtenerDetalles());
+        determinarTipo(empleado1);
         empleado1 = new Gerente("Kevin", 5000, "Sistemas");
-        imprimir(empleado1);
-        //System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+        //determinarTipo(empleado1);
     }
     
-    public static void imprimir(Empleado empleado){
-        String detalles = empleado.obtenerDetalles();
-        System.out.println("detalles = " + detalles);
+    public static void determinarTipo(Empleado empleado){
+        if(empleado instanceof Gerente){
+            System.out.println("Es de tipo Gerente");
+            Gerente gerente = (Gerente)empleado;
+            //((Gerente) empleado).getDepartamento();
+            System.out.println("gerente = "+gerente.getDepartamento());
+        }
+        else if(empleado instanceof Empleado){
+            System.out.println("Es de tipo Empleado");
+            //Gerente gerente = (Gerente)empleado;
+            //System.out.println("gerente = "+gerente.getDepartamento());
+        }
+        else if(empleado instanceof Object){
+            System.out.println("Es de tipo Object");
+        }
     }
 }

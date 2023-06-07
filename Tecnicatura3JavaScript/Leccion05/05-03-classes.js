e1e09b972059420ff44bbc41b1e462580f90e736
FrancoFB <112664039+FrancoFB@users.noreply.github.com>
2023-06-06 23:28:29 -0300

    Creación de constantes estáticas

--- Tecnicatura3JavaScript/Leccion05/05-03-classes.js
+++ Tecnicatura3JavaScript/Leccion05/05-03-classes.js
@@ -3,11 +3,19 @@
 class Persona { //Clase padre
     
     static contadorObjetosPersona = 0; //Atributo estatico
-    email = 'Valor default email'; // Atributo NO estatico
-    
+    //email = 'Valor default email'; // Atributo NO estatico
+ 
+    static get MAX_OBJ(){ //Simula una constante
+    return 5;
+    }
     constructor(nombre, apellido) {
         this._nombre = nombre;
         this._apellido = apellido;
+        if(Persona.contadorPersona < Persona.MAX_OBJ){ 
+    this.idPersona = ++Persona.contadorPersona;
+} else{ 
+    console.log('Se ha superado el maximo de objetos permitidos'); //aparece el mensaje al superarse el limite
+    }
     }
 
     get nombre() {
@@ -104,6 +112,17 @@ console.log(Empleado.contadorObjetosPersona);
 
 //Atributo estatico vs No estatico
 //Atributo estatico se asocia a la Clase y en cuanto al Atributo NO estatico se asocia a los objetos
-console.log(persona1.email); Valor default email //accedemos desde el objeto persona1 de la clase padre al atributo NO estatico
-console.log(empleado1.email); Valor default email //accedemos desde el objeto empleado1 de la clase hija al atributo NO estatico
+console.log(persona1.email); //Valor default email //accedemos desde el objeto persona1 de la clase padre al atributo NO estatico
+console.log(empleado1.email); //Valor default email //accedemos desde el objeto empleado1 de la clase hija al atributo NO estatico
 //console.log(Persona.email); undefined  Se demuestra que no se puede acceder al atributo NO estatico desde la clase debido a que es un objeto estatico, sino que esta creando una nueva variable
+
+//Creacion de constantes estaticas
+//Para esto debemos crear un metodo estatico que solo nos permitira leer el valor que va a regresar este metodo
+console.log(Persona.MAX_OBJ); //Nos regresa al resultado de valor 5
+Persona.MAX_OBJ = 10; //Incluso si quisieramos modificar o alterar el valor de la constante no nos permite 
+console.log(Persona.MAX_OBJ); 
+
+Let persona4 = new Persona('Pedro','Rios');
+console.log(persona4.toString());
+Let persona5 = new Persona('Celeste','Callejas');
+console.log(persona5.toString());

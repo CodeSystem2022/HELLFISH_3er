501e879ab48f6829f577007d47d2cb4fb8b44233
Ana Ascua <83845164+aniascua@users.noreply.github.com>
2023-06-09 13:59:09 -0300

    Update Calculadora.py

--- Proyecto Python/main/calculadora/Calculadora.py
+++ Proyecto Python/main/calculadora/Calculadora.py
@@ -1,5 +1,9 @@
+# Creamos funciones para realizar operaciones aritméticas:
+
+# Función para restar:
 def restar(num1, num2):
         return num1 - num2
 
+# Función para multiplicar
 def multiplicar(num1, num2):
         return num1 * num2

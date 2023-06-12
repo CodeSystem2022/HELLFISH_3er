05f0f2a3a0e8e1ba8c94e3c7ca3db45f3eb9b03f
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-06-12 11:54:35 -0300

    salto de línea y tab

--- Proyecto Python/main/calculadora/Calculadora.py
+++ Proyecto Python/main/calculadora/Calculadora.py
@@ -4,30 +4,30 @@ from math import log, sqrt
 
 
 def sumar(n1, n2):
-    return f"{n1} + {n2} = {n1 + n2}"
+    return f"\n\t{n1} + {n2} = {n1 + n2}"
 
 
 # Función para restar:
 def restar(num1, num2):
-    return f"{num1} - {num2} = {num1 - num2}"
+    return f"\n\t{num1} - {num2} = {num1 - num2}"
 
 
 # Función para multiplicar
 def multiplicar(num1, num2):
-    return f"{num1} * {num2} = {num1 * num2}"
+    return f"\n\t{num1} * {num2} = {num1 * num2}"
 
 
-def division(n1, n2):
-    return f"{n1} / {n2} = {n1 / n2}"
+def dividir(n1, n2):
+    return f"\n\t{n1} / {n2} = {n1 / n2}"
 
 
 def potencia(n1, n2):
-    return f"{n1} ^ {n2} = {n1 ** n2}"
+    return f"\n\t{n1} ^ {n2} = {n1 ** n2}"
 
 
 def raiz(n1):
-    return f"Raiz cuadrada de {n1} = {sqrt(n1)}"
+    return f"\n\tRaiz cuadrada de {n1} = {sqrt(n1)}"
 
 
 def logaritmo(n1):
-    return f"Logaritmo natural de {n1} = {log(n1)}"
+    return f"\n\tLogaritmo natural de {n1} = {log(n1)}"

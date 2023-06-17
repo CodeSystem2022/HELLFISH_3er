b503764c44db5b71814603b2a871caa3341c556e
Maricel Luna <112595886+Macelluna@users.noreply.github.com>
2023-06-16 23:24:07 -0300

    Update TestExcepciones.java

--- Tecnicatura3Java/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
+++ Tecnicatura3Java/Leccion08/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -1,30 +1,3 @@
-
-package test;
-import static aritmetica.Aritmetica.division;
-import excepciones.OperacionExcepcion;
-public class TestExcepciones {
-    public static void main (String [] args ) {
-        int resultado = 0;
-        try{
-            resultado = division(10, 0);   
-        } catch (OperacionExcepcion e) {                                            //Se acomodan las excepcions de manera jerarquica
-            System.out.println("Ocurrió un error de tipo OperacionExcepcion");
-            System.out.println(e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Ocurrio un Error");
-            e.printStackTrace(System.out); // se conoce como la pila de excepciones
-            System.out.println(e.getMessage());
-        }
-        finally {
-            System.out.println("Se revisó la división entre cero");
-        }
-        System.out.println("La variable de resultado tiene como valor:"+resultado);
-    }
-    
-}
-
-//Agrego ejercicio Java 8.3 
-
 package test;
 
 import static aritmetica.Aritmetica.division;
@@ -49,4 +22,5 @@ public class TestExcepciones {
         System.out.println("La variable de resultado tiene como valor: "+resultado);
     }
 }
+
 

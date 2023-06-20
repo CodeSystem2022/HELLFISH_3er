ea91a607a19acd5025271596952bdd661dcd63d8
SofiaaSottile18 <112595421+SofiaaSottile18@users.noreply.github.com>
2023-06-19 22:20:39 -0300

    Clase 10 - Creación clase Persona parte 3

--- Tecnicatura3Java/Leccion11/ListarPersonas/src/main/java/Persona.java
+++ Tecnicatura3Java/Leccion11/ListarPersonas/src/main/java/Persona.java
@@ -17,4 +17,49 @@ public class Persona {
         this.tel = tel;
         this.email = email;
     }
+
+    // Getters
+    public int getId() {
+        return this.id;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getTel() {
+        return this.tel;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona { id = " + this.id + ", nombre = " + this.nombre + ", tel = " + this.tel + ", email = " + this.email + " }";
+    }
+
+    // Prueba del método toString()
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("María", "234882242", "Marimaria@gmail.com");
+        System.out.println(persona1);
+    }
 }

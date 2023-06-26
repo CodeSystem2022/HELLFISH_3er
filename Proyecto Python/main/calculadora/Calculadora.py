703364e7643dbfbd6cb35261699b36e9473d4b39
FrancoFB <112664039+FrancoFB@users.noreply.github.com>
2023-06-26 18:52:15 -0300

    funciones de la Calculadora

--- Proyecto Python/main/calculadora/Calculadora.py
+++ Proyecto Python/main/calculadora/Calculadora.py
@@ -1,3 +1,32 @@
 from math import log, sqrt
 # Creamos funciones para realizar operaciones aritméticas
 # y devolvemos mensajes listos para imprimir
+
+def sumar(n1, n2):
+    return f"\n\t{n1} + {n2} = {n1 + n2}"
+
+
+# Función para restar:
+def restar(num1, num2):
+    return f"\n\t{num1} - {num2} = {num1 - num2}"
+
+
+# Función para multiplicar
+def multiplicar(num1, num2):
+    return f"\n\t{num1} * {num2} = {num1 * num2}"
+
+
+def dividir(n1, n2):
+    return f"\n\t{n1} / {n2} = {n1 / n2}"
+
+
+def potencia(n1, n2):
+    return f"\n\t{n1} ^ {n2} = {n1 ** n2}"
+
+
+def raiz(n1):
+    return f"\n\tRaiz cuadrada de {n1} = {sqrt(n1)}"
+
+
+def logaritmo(n1):
+    return f"\n\tLogaritmo natural de {n1} = {log(n1)}"

712bd0dd90fe5247cfb462c6067b335556ed707e
ludmilailen <112597163+ludmilailen@users.noreply.github.com>
2023-06-26 21:19:29 -0300

    Función para crear base de datos

--- Proyecto Python/main/inventario/conexion.py
+++ Proyecto Python/main/inventario/conexion.py
@@ -1,2 +1,22 @@
 import psycopg2 as bd
 import sys
+
+
+def crear_base_datos():
+    # Creamos base de datos para trabajar
+    conn = bd.connect(
+            host="127.0.0.1",
+            port="5432",
+            user="postgres",
+            password="admin"
+            )
+
+    conn.autocommit = True
+    cursor = conn.cursor()
+
+    cursor.execute("CREATE DATABASE hellfish3")
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()

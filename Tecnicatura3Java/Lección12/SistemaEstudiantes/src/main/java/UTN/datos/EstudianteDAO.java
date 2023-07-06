77c9724e0f638f83230951a32c78a2a48da49bf0
AgusAdTello <112036990+AgusAdTello@users.noreply.github.com>
2023-07-05 22:28:47 -0300

    Java clase 12

--- Tecnicatura3Java/Lección12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
+++ Tecnicatura3Java/Lección12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -13,14 +13,14 @@ import java.util.List;
 
 public class EstudianteDAO {
     // Método Listar:
-    public List<Estudiante> listar(){
+    public List<Estudiante> listarEstudiantes(){
         List<Estudiante> estudiantes = new ArrayList<>();
         // Creamos algunos objetos que son necesarios para comunicarnos con la base de datos:
         PreparedStatement ps; // Ayuda a preparar la sentencia SQL y la envia a la base de datos
         ResultSet rs; // Obtiene el resultado de la consulta SQL en la base de datos
         //Creamos un objeto de tipo Conexion
         Connection con = getConnection();
-        String sql = "SELECT * FROM estudiantes2022 ORDER BY estudiantes2022";
+        String sql = "SELECT * FROM estudiantes2022 ORDER BY idestudiantes2022";
         try{
             ps = con.prepareStatement(sql);
             rs = ps.executeQuery();
@@ -49,13 +49,60 @@ public class EstudianteDAO {
 
         } //Fin del metodo listar
 
+//12.4 Creamos en PersonaDAO() el método -> fin by id (buscarEstudiantePorId())
+
+//Metodo por id -> fin by id
+public boolean buscarEstudiantePorID(Estudiante estudiante){
+    PreparedStatement ps;
+    ResultSet rs;
+    Connection con = getConnection();
+    String sql = "SELECT * FROM estudiantes2022 WHERE idestudiantes2022=?";
+    try{
+        ps = con.prepareStatement(sql);
+        ps.setInt(1, estudiante.getIdEstudiante());
+        rs = ps.executeQuery();
+        if(rs.next()){
+            estudiante.setNombre(rs.getString("nombre"));
+            estudiante.setApellido(rs.getString("Apellido"));
+            estudiante.setTelefono(rs.getString("Telefono"));
+            estudiante.setEmail(rs.getString("Email"));
+            return_tru; //Se encontro un registro
+
+        }//Fin if
+    } catch (Exception e){
+        System.out.println("Ocurrio un error al buscar estudiante: "+e.getMessage());
+    }//Fin catch
+    finally {
+        try{
+            con.close();
+        }catch (Exception e){
+            System.out.println("Ocurrio un error al cerrar la conexion: "+e.get.getMessage());
+        }//Fin catch
+    }//Fin finally
+    return false;
+}
+//12.3 Comenzamos con las pruebas del método listarEstudiantes() -> solucionamos algunos errores de conexión
+public static void main(String[] args) {
+    //Listar los Estudiantes
+    var estudianteDao = new EstudianteDAO();
+    System.out.println("Listado de Estudiantes");
+    List<Estudiante> estudiantes = estudianteDao.listarEstudiantes()
+    estudiantes.forEach(System.out::println); //Funcion lamda para imprimir
+
 // 12.5 Hacemos las pruebas del método -> Buscar un estudiante por ID()
-//        var estudiante1 = new Estudiante(1);
-//        System.out.println("Estudiantes antes de la búsqueda: "+estudiante1);
-//        var encontrado = estudianteDao.buscarEstudiantePorID(estudiante1);
-//        if(encontrado)
-//            System.out.println("Estudiante encontrado: "+estudiante1);
-//        else
-//            System.out.println("No se encontró al estudiante: "+estudiante1.getIdEstudiante());
-    }
+    
+    var estudiante1 = new Estudiante(1);
+    System.out.println("Estudiantes antes de la búsqueda: "+estudiante1);
+    var encontrado = estudianteDao.buscarEstudiantePorID(estudiante1);
+
+    if(encontrado)
+        System.out.println("Estudiante encontrado: "+estudiante1);
+    else
+        System.out.println("No se encontró al estudiante: "+estudiante1.getIdEstudiante());
+
+}
+
+@Override
+public String toString() {
+    return "EstudianteDAO []";
 }
